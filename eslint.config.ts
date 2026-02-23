0c63f11fbd5659201687d2ca8563d788dea740dc
Dolan Murvihill <dolan.murvihill@downstream.com>
2026-02-23 11:40:43 -0800

    Fix lint warning

--- eslint.config.ts
+++ eslint.config.ts
@@ -2,7 +2,7 @@ import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 import json from "@eslint/json";
-import markdown from "@eslint/markdown";
+// import markdown from "@eslint/markdown";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
